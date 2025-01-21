d4454f5569b82748467babd41f6018ccfcbc21aa
Hesch <heschmat.seyedi@gmail.com>
2025-01-21 01:59:37 +0100

    Setup a basic HTTP server.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Application version
+//@TODO: later generate this automatically at build time.
+const version = "1.0.0"
+
+// The *config* struct holds all the configuration settings for the application.
+// We will read in *configuration settings* from the command-line flags when the application starts.
+// port: the network port that we want the server to listen on
+// env : the operating environment for the application (development, staging, production)
+// ...
+type config struct {
+	port int
+	env  string
+}
+
+// The *application* struct holds all the `dependencies` for the HTTP handlers, helpers & middleware.
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
 
 func main() {
-	fmt.Println("Hello world!")
+	// Declare an instance of the config struct.
+	var cfg config
+
+	// Read the command-line flags into the config struct.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Inisitalize a new structured logger.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Declare an instance of the application struct.
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	// Start the HTTP server.
+	logger.Info("Starting server", "addr", srv.Addr, "env", cfg.env)
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
